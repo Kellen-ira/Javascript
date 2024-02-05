66cfaa5f9d6fe140c37044dd2cac973ed6d6dfae
kellen-ira <ikellen2016@gmail.com>
2024-02-05 16:43:49 +0200

    done

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// let maximum;
+//      maximum=Math.max.apply(null, [1, 2, 3, 1000]);
+//      document.write(maximum);
+
+
+
+// function sum(q,w)
+// {
+//    let sum=q+w;
+//    return sum;
+// }
+// console.log(sum(10,50));
+
+        
+
+//  const striReturn =(str) =>
+//  {
+//      if (isNaN(str) && str.includes("w") && str.length === 8)
+//      {
+//          return true;
+//      }
+//      else{
+//          return false;
+//      }
+//  }
+//  console.log(striReturn("whhhddrrrsxs"));
+
+
+// const currentdate = new Date();
+// const dayIndex =currentdate.getDay();
+// const daysofweek =["Monday","Tuesday","Wednesday","Thursday","Friday","Suturday","Sunday"];
+// const dayofweek = daysofweek[dayIndex];
+// console.log("To Day Is :"+daysofweek[1]);
+
+// console.log("To Day Is: " + dayofweek + ".");
+
+const a=5;
+const b=6;
+ const c=7;
+AreaOfTriangle=a+b+c/2;
+console.log(AreaOfTriangle);
---
+++ index2.js
@@ -0,0 +1,38 @@
+//const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+
+//var colorjoined = colors.join(", ");
+
+//nconsole.log(colorjoined);
+
+//output
+//red,green,blue,yellow,orange
+
+
+
+
+//const removed = colors.splice(0,2, "purple", "pink"); 
+//console.log(removed);
+//console.log(colors);
+
+//output
+//[ 'purple', 'pink', 'blue', 'yellow', 'orange' ]
+
+
+//let copy = colors.copyWithin(2,0,2);
+//console.log(copy);
+//console.log(colors);
+
+//output
+//[ 'red', 'green', 'red', 'green', 'orange' ]
+
+
+const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+//let result = colors.map(Element => {return Element.toUpperCase()});
+//console.log(result);
+  //ouput
+//[ 'RED', 'GREEN', 'BLUE', 'YELLOW', 'ORANGE' ]
+
+ let colorfind = color.find();
+ console.log()
+
+
